f53b3a4abe33f4c84db4c5d594791a195696030b
Jeff Pittman <geojeff@me.com>
2012-02-25 20:56:54 -0600

    Moved addTodo from the todosController to the statechart. Added a new READY_APP state to hold addTodo.

--- apps/todos/controllers/todos.js
+++ apps/todos/controllers/todos.js
@@ -2,14 +2,4 @@ require('core');
  
 // A controller that will provide a list of all of the todos.
 Todos.todosController = SC.ArrayController.create({
-  addTodo: function (view) {
-    var todo = (view.get('value') || '').trim();
-    if (todo !== '') {
-      Todos.store.createRecord(Todos.Todo, {
-        title: todo,
-        timestamp: SC.DateTime.create()
-      });
-      view.set('value', '');
-    }
-  }
 });
--- apps/todos/statechart.js
+++ apps/todos/statechart.js
@@ -76,14 +76,56 @@ Todos.statechart = SC.Statechart.create({
         // Step 3. Once the page comes up, put keyboard focus on the new todos item.
         //
         Todos.mainPage.getPath('mainPane.newTodoField.field').becomeFirstResponder();
+
+        // Done with loading and initialization steps. Move to READY_APP.
+        this.gotoState('READY_APP');
       },
  
       exitState: function() {
+        // Nothing to worry about in this context.
+      }
+    }),
+     
+    // The mainPage view is now showing, after being appended in the LOADING_APP state.
+    // So now we are in the READY state, waiting for and responding to user actions.
+    //
+    READY_APP: SC.State.design({
+      enterState: function() {
+        // There could be a user interface update for ready status after loading,
+        // such as a status indicator that turns from a yellow to green light, but
+        // we don't have anything fancy like that. So, nothing to worry about. Just
+        // stand at the ready.
+      },
+
+      exitState: function() {
         // In a larger app, we would do tear-down tasks here.
+      },
+
+      // Functions like addTodo are called actions. Their names are used to set the
+      // action property of user interface elements such as buttons. In this case,
+      // the mainPage view has a newTodoField view that contains a text field and
+      // a submit button. The action property of the submit button is set to 'addTodo'.
+      // When clicked, this addTodo function will be called.
+      //
+      // If the user entered some text, which is grabbed from the value property of
+      // the associated view of the caller (a text field), addTodo uses this and the
+      // current time in a call to store.createRecord() to add a new todo.
+      //
+      // As the last step, the associated view's value is set to '', so the text
+      // field is blank and ready for the next todo.
+      //
+      addTodo: function (view) {
+        var todo = (view.get('value') || '').trim();
+        if (todo !== '') {
+          Todos.store.createRecord(Todos.Todo, {
+            title: todo,
+            timestamp: SC.DateTime.create()
+          });
+          view.set('value', '');
+        }
       }
     })
- 
+
     // More states would go here in a bigger app.
-     
   })
 });
