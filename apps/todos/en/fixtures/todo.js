3848642da5db8fac2e9ee89f21f6cc6113f1408d
Jeff Pittman <geojeff@me.com>
2012-02-25 21:21:00 -0600

    Added three todos to fixtures.

--- apps/todos/en/fixtures/todo.js
+++ apps/todos/en/fixtures/todo.js
@@ -4,6 +4,16 @@ Todos.Todo.FIXTURES = [
  {
    guid: 0,
    isCompleted: YES,
-   title: 'Finish Todo app'
+   title: 'Take out the garbage'
+ },
+ {
+   guid: 1,
+   isCompleted: YES,
+   title: 'Walk the dog'
+ },
+ {
+   guid: 2,
+   isCompleted: YES,
+   title: 'Wash the dishes'
  }
 ];
